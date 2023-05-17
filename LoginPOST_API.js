c464b80f873ecf16f6ca088dfd22aa2725f2880f
Your Name <you@example.com>
2023-05-17 17:57:56 +0530

    Login_POST_API

---
+++ LoginPOST_API.js
@@ -0,0 +1,28 @@
+function logininfo(){
+    let username=document.querySelector('.username').value;
+    let password=document.querySelector('.pswd').value;
+
+    let details={
+        "username" : username,
+        "password" : password
+    };
+    //Dharani's (server) system IP address
+    fetch("http://192.168.0.153:5000/info",{
+        method:"POST", 
+        body:JSON.stringify(details),
+        headers:{
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res =>{
+        return res.json()
+    })
+    .then(data=> {
+        console.log(data)
+        
+    }).catch((err)=>{
+        console.log(err,err) 
+    });
+}
+
+
